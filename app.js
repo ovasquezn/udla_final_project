b89817f446255b8d371debf49877c091af52e6cd
ovasquez <orlandovasquezneira@gmail.com>
2024-11-15 00:10:36 -0300

    Test 3

--- app.js
+++ app.js
@@ -42,8 +42,8 @@ app.use('/finanzas', financesRoutes);
 
 app.use('/configuraciones', configRoutes)
 
-const PORT = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
-app.listen(PORT,"0.0.0.0", () => {
-    console.log(`Server on port ${PORT}`);
-}  );
+app.listen(port,"0.0.0.0", function () {
+    console.log(`Server on port ${port}`);
+});
